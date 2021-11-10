b26f90e293f78634c45f155e8da8c8b2d8046113
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-10 09:24:40 +0100

    find missing number in array from 1 to 10

---
+++ Open-Lab-04.12/Open-Lab-04.12/Numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Open_Lab_04._12
+{
+    public class Numbers
+    {
+        public int MissingNum(int[] nums)
+        {
+            var all = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            
+          foreach(int i in nums)
+            {
+                all.Remove(i);
+
+            }
+
+            int vysledok = all[0];
+
+            return vysledok;
+        }
+    }
+}
